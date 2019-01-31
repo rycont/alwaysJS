588eab8a741b1ecda4841037074adc9e95787a73
unknown <wjdgks1224@gmail.com>
2019-01-31 20:08:48 +0900

    connot loaded contribution.js

--- vs/basic-languages/perl/perl.contribution.js
+++ vs/basic-languages/perl/perl.contribution.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "../_.contribution"], function (require, exports, __contribution_1) {
+define(["require", "exports", "../contribution"], function (require, exports, __contribution_1) {
     /*---------------------------------------------------------------------------------------------
     *  Copyright (c) Microsoft Corporation. All rights reserved.
     *  Licensed under the MIT License. See License.txt in the project root for license information.
